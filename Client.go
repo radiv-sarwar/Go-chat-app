255deb7f1dc2d7d10a1200706258f9a4b2723705
radiv-sarwar <radiv.sarwar7@gmail.com>
2025-05-19 10:36:56 +0600

    Am able to send messages to the server from the client now. Added comments to remind me about what the code does in case I need to revise.

--- Client.go
+++ Client.go
@@ -1,17 +1,44 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"log"
 
+	"github.com/gorilla/websocket"
+)
+
+// The Client structure will take care of the client connecting to the server
 type Client struct {
 	connection *websocket.Conn
 	manager    *Manager
 }
 
+// A map of Clients that the manager is handling.
 type ClientList map[*Client]bool
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager,
+	}
+}
+
+func (c *Client) readMessages() {
+	defer func() {
+		// cleanup the connection
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		messageType, payload, err := c.connection.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading message: %v", err)
+			}
+			break
+		}
+
+		log.Println(messageType)
+		log.Println(string(payload))
+
 	}
 }
--- manager.go
+++ manager.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Setting a variable to use to upgrade a http connection to a websocket conn
 var (
 	websocketUpgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -15,17 +16,26 @@ var (
 	}
 )
 
+// This is the manager struct. Everytime a client connects, it gets assigned to a manager.
+// This manager saves the client in its own Clients map.
 type Manager struct {
 	clients ClientList
 	sync.RWMutex
 }
 
+// Method to return an empty manager struct (struct starts with a zero values) when initializing.
 func NewManager() *Manager {
-	return &Manager{}
+	return &Manager{
+		clients: make(ClientList),
+	}
 }
 
+// This is the main function. This will start everything by establishsing a websocket connection
+// with the client.
 func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
-	log.Println("new connection")
+
+	// Declaring the start of a new websocket connection.
+	log.Println("Starting a new websocket connection")
 
 	//upgrade to websocket
 	conn, err := websocketUpgrader.Upgrade(w, r, nil)
@@ -33,18 +43,26 @@ func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
+	// Adding the client to the manager's client list.
 	client := NewClient(conn, m)
 	m.addClient(client)
 
+	// start two go routines per client
+	// one is to read messages and one is to write messages
+	go client.readMessages()
+
 }
 
+// Function to add clients to the clients list in our manager
 func (m *Manager) addClient(client *Client) {
 	m.Lock()
 	defer m.Unlock()
 
+	// Adding the client to the ClientList
 	m.clients[client] = true
 }
 
+// Function to remove the client from the client list in our manager struct
 func (m *Manager) removeClient(client *Client) {
 	m.Lock()
 	defer m.Unlock()
